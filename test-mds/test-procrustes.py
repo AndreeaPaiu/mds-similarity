6608761e747cdf9a1133ccbf698fb1273216fc24
Andreea Paiu <paiuandreea18@yahoo.com>
2024-04-09 10:27:51 +0300

    [09.04.21024] add test procrustes

--- test-mds/test-procrustes.py
+++ test-mds/test-procrustes.py
@@ -7,7 +7,8 @@ from numpy import linalg as LA
 # Generate two sets of 2D points as matrices
 matrix1 = np.array([[1, 0], [2, 0], [2, 1], [1, 1], [1, 0]])
 matrix2 = np.array([[1, 1], [2, 0], [3, 1], [2, 2], [1, 1]])
-matrix3 = np.array([[1, 0], [3, 0], [3, 2], [1, 2], [1, 0]])
+matrix3 = np.array([[1, 1], [3, 0], [3, 2], [1, 3], [1, 1]])
+matrix4 = np.array([[1, 0], [3, 0], [3, 2], [1, 3], [1, 0]])
 
 # # Perform Procrustes analysis to align matrix2 to matrix1
 # R, s = orthogonal_procrustes(matrix1, matrix2)
@@ -15,7 +16,15 @@ matrix3 = np.array([[1, 0], [3, 0], [3, 2], [1, 2], [1, 0]])
 # mtx2_aligned = matrix2@R.T
 
 mtx1, mtx2, disparity = procrustes(matrix1, matrix2)
+print('1')
+print(mtx1)
 mtx1, mtx3, disparity = procrustes(matrix1, matrix3)
+print('2')
+print(mtx1)
+mtx1, mtx4, disparity = procrustes(matrix1, matrix4)
+print('3')
+print(mtx1)
+
 
 # Plot the original and aligned matrices
 plt.figure()
@@ -25,6 +34,8 @@ plt.subplot(1, 2, 1)
 plt.plot(matrix1[:, 0], matrix1[:, 1], 'bo-', label='Matrix 1')
 plt.plot(matrix2[:, 0], matrix2[:, 1], 'ro-', label='Matrix 2')
 plt.plot(matrix3[:, 0], matrix3[:, 1], 'go-', label='Matrix 3')
+plt.plot(matrix4[:, 0], matrix4[:, 1], 'yo-', label='Matrix 4')
+
 plt.title('Original Matrices')
 plt.legend()
 
@@ -33,6 +44,8 @@ plt.subplot(1, 2, 2)
 plt.plot(mtx1[:, 0], mtx1[:, 1], 'bo-', label='Matrix 1 Aligned')
 plt.plot(mtx2[:, 0], mtx2[:, 1], 'ro-', label='Matrix 2 Aligned')
 plt.plot(mtx3[:, 0], mtx3[:, 1], 'go-', label='Matrix 3 Aligned')
+plt.plot(mtx4[:, 0], mtx4[:, 1], 'yo-', label='Matrix 4 Aligned')
+
 plt.title('Aligned Matrices')
 plt.legend()
 
