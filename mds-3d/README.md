5ce9e8a224b7aa6a9e561c76b2ecd83ee4e302f8
Andreea Paiu <paiuandreea18@yahoo.com>
2024-12-01 22:21:05 +0200

    [1.12.2024] save work

--- mds-3d/README.md
+++ mds-3d/README.md
@@ -11,3 +11,56 @@ processing_required_data: structura date
     nr_ctr + collection -> cartesian_coordinates    -> [x, y, z]
                         -> wifi                     -> [{mac: rssi}]
 
+# stress 
+calcul ->   in smacof este calculat ca stress = ((dist.ravel() - similarities.ravel()) ** 2).sum() / 2, noi am calculat si doar
+            diferente distante (dist.ravel() - similarities.ravel()), respectiv ((dist.ravel() - similarities.ravel()) ** 2), dar
+            si deviatia strandard
+
+testare comportament 
+    -> setul de date data.upb\precis\02-precis-redmi-pixel4a\ 0-pixel-04-06-2021_19-34-41.json 0-redmi-04-06-2021_19-33-47.json
+    -> prima data am rulat cele doua seturi de date pentru a observa stresul si am colectat urmatoarele date:
+        SET 1
+        Stress_value = 15.738369617744091
+        n_iter = 25
+        Stresul mediu = 15.735892697246856
+        Stress on one pair: 0.004460287045704891
+        Difference between point -> standard deviation: 0.03490771650586993
+        images/raport3/histogram-stress-wifi-points-set0-84points
+        Smacof example stress -> standard deviation: 0.004353379821758031
+        
+        SET 2
+        Stress_value = 15.747779640160733
+        n_iter = 18
+        Stresul mediu = 15.743925025008158
+        Stress on one pair: 0.004462563782598684
+        Difference between point -> standard deviation: 0.03363247776433891
+        Smacof example stress -> standard deviation: 0.004226581177592235
+
+    -> in urmatorul pas am utilizat impreuna ambele seturi si am colectat datele
+        Ambele seturi
+        Stress_value = 62.26587304322902
+        n_iter = 20
+        Stresul mediu = 62.26023338953856
+        Stress on one pair: 0.00441186461093669
+        Difference between point -> standard deviation: 0.03390966960796572
+        Smacof example stress -> standard deviation: 0.0042349780950368355
+    -> cu jumatate din punctele din primul set 
+        Stress_value = 3.9613448112652474
+        n_iter = 15
+        Stresul mediu = 3.9600477597779227
+        Stress on one pair: 0.0044898500677754225
+        Difference between point -> standard deviation: 0.035307427412994974
+        Smacof example stress -> standard deviation: 0.004354454760001887
+    -> un sfert din punctele din primul set
+        Stress_value = 0.9612255099971188
+        n_iter = 21
+        Stresul mediu = 0.9606047354993383
+        Stress on one pair: 0.004356484061221489
+        Difference between point -> standard deviation: 0.035281086736370114
+        Smacof example stress -> standard deviation: 0.0042293366955556315
+    -> 1/6
+        Stresul mediu = 0.4486601780541596
+        Stress on one pair: 0.004578165082185302
+        Difference between point -> standard deviation: 0.03650339824710645
+        images/raport3/histogram-stress-wifi-points-1-6-set0-84points
+        Smacof example stress -> standard deviation: 0.004324595427753183
--- mds-3d/main.py
+++ mds-3d/main.py
@@ -205,23 +205,23 @@ if __name__ == '__main__':
 #             nr_clusters = 2
 #         )
 #
-#        2 etaje din 1 in 1
-    type_data = 'wifi'
-    for i in range(len(data)):
-        if i == (len(data) - 1):
-            break
-
-        show_similarities_histogram(
-            data[i],
-            data[i+1],
-            type_data=type_data,
-            path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+1}.png',
-            title=f'Similarities {type_data} points floor{i} and floor{i+1}',
-            xlabel='Values',
-            ylabel='Density',
-            simil_method=cosine,
-            selection='All'
-        )
+# #        2 etaje din 1 in 1
+#     type_data = 'wifi'
+#     for i in range(len(data)):
+#         if i == (len(data) - 1):
+#             break
+#
+#         show_similarities_histogram(
+#             data[i],
+#             data[i+1],
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+1}.png',
+#             title=f'Similarities {type_data} points floor{i} and floor{i+1}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All'
+#         )
 
 #       2 etaje din 1 in 1
 #     type_data = 'cartesian'
@@ -241,52 +241,264 @@ if __name__ == '__main__':
 #             selection='All'
 #         )
 
-#     2 etaje din 2 in 2
+# #     2 etaje din 2 in 2
+#     type_data = 'wifi'
+#     for i in range(len(data)):
+#         if i == (len(data) - 2):
+#             break
+#
+#         show_similarities_histogram(
+#             data[i],
+#             data[i+2],
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+2}.png',
+#             title=f'Similarities {type_data} points floor{i} and floor{i+2}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All'
+#         )
+
+# #   2 etaje din 2 in 2
+#     type_data = 'cartesian'
+#     for i in range(len(data)):
+#         if i == (len(data) - 2):
+#             break
+#
+#         show_similarities_histogram(
+#             data[i],
+#             data[i+2],
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+2}.png',
+#             title=f'Similarities {type_data} points floor{i} and floor{i+2}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All'
+#         )
+
+
+#     type_data = 'wifi'
+#     for i in range(len(data)):
+#         show_stress_histogram(
+#             data[i],
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-stress-{type_data}-points-floor{i}',
+#             title=f'Stress {type_data} points floor{i}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All',
+#             extension='.png'
+#         )
+
+
+##   similaritati intr-un etaj
+#     type_data = 'wifi'
+#     for i in range(len(data)):
+#         show_similarities_histogram(
+#             data[i],
+#             None,
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}.png',
+#             title=f'Similarities {type_data} points floor{i}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All'
+#         )
+
+# #   similaritati intr-un etaj
+#     type_data = 'cartesian'
+#     for i in range(len(data)):
+#         show_similarities_histogram(
+#             data[i],
+#             None,
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}.png',
+#             title=f'Similarities {type_data} points floor{i}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All'
+#         )
+
+# #   testez comportamentul stresului (schimba setul de date)
+#     type_data = 'wifi'
+#     for i in range(len(data)):
+#         show_stress_histogram(
+#             data[i],
+#             type_data=type_data,
+#             path=f'images/raport3/histogram-stress-{type_data}-points-set{i}-84points',
+#             title=f'Stress {type_data} points set{i}',
+#             xlabel='Values',
+#             ylabel='Density',
+#             simil_method=cosine,
+#             selection='All',
+#             extension='.png'
+#         )
+#
+#     type_data = 'wifi'
+#     type_plot = 'mds'
+#     dimension = 2
+#     for i in range(len(data)):
+#         show_data(
+#             data[i],
+#             type_data=type_data,
+#             type_plot=type_plot,
+#             dimension=dimension,
+#             path=f'images/raport3/{type_plot}-{dimension}D-{type_data}-points-set{i}-84points.png',
+#             title=f'{type_plot} {dimension}D {type_data} points set{i} 84points',
+#             xlabel='x',
+#             ylabel='y',
+#             zlabel='z',
+#             simil_method=cosine,
+#             selection='All',
+#             nr_clusters = 2
+#         )
+#
+# #      both datasets
+#     data_s = {}
+#     data_s.update(data[0])
+#     data_s.update(data[1])
+#     type_data = 'wifi'
+#     show_stress_histogram(
+#         data_s,
+#         type_data=type_data,
+#         path=f'images/raport3/histogram-stress-{type_data}-points-both-set{i}-84points',
+#         title=f'Stress {type_data} points set{i}',
+#         xlabel='Values',
+#         ylabel='Density',
+#         simil_method=cosine,
+#         selection='All',
+#         extension='.png'
+#     )
+#     type_data = 'wifi'
+#     type_plot = 'mds'
+#     dimension = 2
+#     show_data(
+#         data_s,
+#         type_data=type_data,
+#         type_plot=type_plot,
+#         dimension=dimension,
+#         path=f'images/raport3/{type_plot}-{dimension}D-{type_data}-points-both-set{i}-84points.png',
+#         title=f'{type_plot} {dimension}D {type_data} points set{i} 84points',
+#         xlabel='x',
+#         ylabel='y',
+#         zlabel='z',
+#         simil_method=cosine,
+#         selection='All',
+#         nr_clusters = 2
+#         )
+
+# #   half dataset
+#     type_data = 'wifi'
+#     data_s = {}
+#     for i, key in enumerate(data[0]):
+#         if i % 2 == 0:
+#             data_s.update({key: data[0][key]})
+#
+#     show_stress_histogram(
+#         data_s,
+#         type_data=type_data,
+#         path=f'images/raport3/histogram-stress-{type_data}-points-half-set{0}-84points',
+#         title=f'Stress {type_data} points set{0}',
+#         xlabel='Values',
+#         ylabel='Density',
+#         simil_method=cosine,
+#         selection='All',
+#         extension='.png'
+#     )
+#
+#     type_data = 'wifi'
+#     type_plot = 'mds'
+#     dimension = 2
+#     show_data(
+#         data_s,
+#         type_data=type_data,
+#         type_plot=type_plot,
+#         dimension=dimension,
+#         path=f'images/raport3/{type_plot}-{dimension}D-{type_data}-points-half-set{0}-84points.png',
+#         title=f'{type_plot} {dimension}D {type_data} points set{0} 84points',
+#         xlabel='x',
+#         ylabel='y',
+#         zlabel='z',
+#         simil_method=cosine,
+#         selection='All',
+#         nr_clusters = 2
+#         )
+
+#   quarter dataset
+#     type_data = 'wifi'
+#     data_s = {}
+#     for i, key in enumerate(data[0]):
+#         if i % 4 == 0:
+#             data_s.update({key: data[0][key]})
+#
+#     show_stress_histogram(
+#         data_s,
+#         type_data=type_data,
+#         path=f'images/raport3/histogram-stress-{type_data}-points-quarter-set{0}-84points',
+#         title=f'Stress {type_data} points set{0}',
+#         xlabel='Values',
+#         ylabel='Density',
+#         simil_method=cosine,
+#         selection='All',
+#         extension='.png'
+#     )
+#
+#     type_data = 'wifi'
+#     type_plot = 'mds'
+#     dimension = 2
+#     show_data(
+#         data_s,
+#         type_data=type_data,
+#         type_plot=type_plot,
+#         dimension=dimension,
+#         path=f'images/raport3/{type_plot}-{dimension}D-{type_data}-points-quarter-set{0}-84points.png',
+#         title=f'{type_plot} {dimension}D {type_data} points set{0} 84points',
+#         xlabel='x',
+#         ylabel='y',
+#         zlabel='z',
+#         simil_method=cosine,
+#         selection='All',
+#         nr_clusters = 2
+#         )
+
+
     type_data = 'wifi'
-    for i in range(len(data)):
-        if i == (len(data) - 2):
-            break
+    data_s = {}
+    for i, key in enumerate(data[0]):
+        if i % 6 == 0:
+            data_s.update({key: data[0][key]})
 
-        show_similarities_histogram(
-            data[i],
-            data[i+2],
-            type_data=type_data,
-            path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+2}.png',
-            title=f'Similarities {type_data} points floor{i} and floor{i+2}',
-            xlabel='Values',
-            ylabel='Density',
-            simil_method=cosine,
-            selection='All'
-        )
-
-#   2 etaje din 2 in 2
-    type_data = 'cartesian'
-    for i in range(len(data)):
-        if i == (len(data) - 2):
-            break
-
-        show_similarities_histogram(
-            data[i],
-            data[i+2],
-            type_data=type_data,
-            path=f'images/raport3/histogram-similarities-{type_data}-points-floor{i}-floor{i+2}.png',
-            title=f'Similarities {type_data} points floor{i} and floor{i+2}',
-            xlabel='Values',
-            ylabel='Density',
-            simil_method=cosine,
-            selection='All'
-        )
-
+    show_stress_histogram(
+        data_s,
+        type_data=type_data,
+        path=f'images/raport3/histogram-stress-{type_data}-points-1-6-set{0}-84points',
+        title=f'Stress {type_data} points set{0}',
+        xlabel='Values',
+        ylabel='Density',
+        simil_method=cosine,
+        selection='All',
+        extension='.png'
+    )
 
     type_data = 'wifi'
-    for i in range(len(data)):
-        show_stress_histogram(
-            data[i],
-            type_data=type_data,
-            path=f'images/raport3/histogram-stress-{type_data}-points-floor{i}.png',
-            title=f'Stress {type_data} points floor{i}',
-            xlabel='Values',
-            ylabel='Density',
-            simil_method=cosine,
-            selection='All'
+    type_plot = 'mds'
+    dimension = 2
+    show_data(
+        data_s,
+        type_data=type_data,
+        type_plot=type_plot,
+        dimension=dimension,
+        path=f'images/raport3/{type_plot}-{dimension}D-{type_data}-points-1-6-set{0}-84points.png',
+        title=f'{type_plot} {dimension}D {type_data} points set{0} 84points',
+        xlabel='x',
+        ylabel='y',
+        zlabel='z',
+        simil_method=cosine,
+        selection='All',
+        nr_clusters = 2
         )
+
